1b2f3a1f8c94b9a36b022a6ecfd1fb5a0ea6706b
python <python@debian>
2020-11-21 00:43:22 -0500

    Pruebas

--- listas_dinamicas.py
+++ listas_dinamicas.py
@@ -1,14 +1,14 @@
-lis_1=list(range(0,11)) #intervalo por defecto es 1
+lis_1 = list(range(0, 11))  # intervalo por defecto es 1
 print(lis_1)
 
-list_2=list(range(-10,1))
+list_2 = list(range(-10, 1))
 print(list_2)
 
-list_3=list(range(0,21,2))
+list_3 = list(range(0, 21, 2))
 print(list_3)
 
-list_4=list(range(-19,0,2))
+list_4 = list(range(-19, 0, 2))
 print(list_4)
 
-list_5=list(range(0,51,5))
+list_5 = list(range(0, 51, 5))
 print(list_5)
